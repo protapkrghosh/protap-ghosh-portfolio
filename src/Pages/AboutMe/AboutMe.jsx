a936000361d586d07cb018b447a8c34c2df95e90
Protap Ghosh <protapg518@gmail.com>
2023-06-28 04:42:26 +0600

    style about me

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,6 +1,7 @@
 import { FaDownload } from "react-icons/fa";
 import Container from "../../Components/Container";
 import image from "../../assets/protap.png"
+import CountUp from "react-countup";
 
 const AboutMe = () => {
    const handleDownload = () => {
@@ -17,7 +18,7 @@ const AboutMe = () => {
             </div>
 
             <div className="md:flex items-center mt-10">
-               <div className="md:w-1/2" data-aos="zoom-in-up" data-aos-duration="1500">
+               <div className="md:w-1/2 mb-10" data-aos="zoom-in-up" data-aos-duration="1500">
                   <img src={image} alt="Image" className="border border-[#20C997] w-[300px] h-auto rounded-xl mx-auto shadow-lg shadow-[#20C997]"/>
                </div>
 
@@ -32,9 +33,42 @@ const AboutMe = () => {
                      I invest a significant amount of time in staying up-to-date with the latest trends and honing my skills. If you have an exciting project in mind, I&apos;m all ears! Feel free to explore my resume below to gain a better understanding of my skills and experience. Let&apos;s connect and bring your project to life together!
                   </p>
 
+                  <div className="grid grid-cols-3 mt-8 mb-5 text-center">
+                     <div>
+                        <span className="text-[#909294] text-5xl font-bold">
+                           <CountUp
+                              end={1}
+                              duration={5}
+                           />+
+                        </span>
+                        <p className="text-white text-xl">Years Experiance</p>
+                     </div>
+
+                     <div>
+                        <span className="text-[#909294] text-5xl font-bold">
+                           <CountUp
+                              end={15}
+                              duration={5}
+                           />+
+                        </span>
+                        <p className="text-white text-xl">Completed Projects</p>
+                     </div>
+
+                     <div>
+                        <span className="text-[#909294] text-5xl font-bold">
+                           <CountUp
+                              end={4}
+                              duration={8}
+                           />+
+                        </span>
+                        <p className="text-white text-xl">Happy Clients</p>
+                     </div>
+                  </div>
+                  
                   <div className="flex justify-center" data-aos="zoom-in" data-aos-duration="1000">
                      <button onClick={handleDownload} className='btn btn-outline border-[#6C757D] hover:border-[#6C757D] text-[16px] text-[#6C757D] capitalize border-2 rounded-full px-8 mt-5 hover:bg-[#6C757D]'>Download Resume <FaDownload /> </button>
                   </div>
+
                </div>
             </div>
          </Container>
