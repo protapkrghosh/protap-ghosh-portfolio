365c332641a48ca9d85fbce859d19eeac49187af
Protap Ghosh <protapg518@gmail.com>
2023-07-04 02:18:54 +0600

    hosting project

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h8m-8 6h20" /></svg>
                </label>
 
-               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-4 z-[1] p-2 space-y-4 shadow bg-base-100 rounded-md w-72">
+               <ul tabIndex={0} className="menu menu-md dropdown-content mt-[14px] z-[1] p-2 space-y-4 shadow text-[#909da7] bg-[#343A40] rounded-md w-[360px]">
                   {navItems}
                </ul>
             </div>
