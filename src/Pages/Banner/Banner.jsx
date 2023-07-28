59bf840215f6910a11f15fc23aa8ce179e75f3e6
Protap Ghosh <protapg518@gmail.com>
2023-07-28 20:57:29 +0600

    added notfound page

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -11,162 +11,162 @@ import NavBar from "../../Shared/NavBar/NavBar";
 import Headroom from "react-headroom";
 
 const Banner = () => {
-   const particlesInit = useCallback(async engine => {
-      console.log(engine);
-      await loadFull(engine);
-   }, []);
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
 
-   const particlesLoaded = useCallback(async container => {
-      await console.log(container);
-   }, []);
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
 
-   const [text] = useTypewriter({
-      words: ['MERN Stack Developer', 'Font-end Developer', 'Junior Web Developer'],
-      loop: {},
-   });
+  const [text] = useTypewriter({
+    words: ['MERN Stack Developer', 'Font-end Developer', 'Junior Web Developer'],
+    loop: {},
+  });
 
-   const handleDownload = () => {
-      window.open('https://drive.google.com/u/1/uc?id=1Q7wzCfqI_aT_PwkPPqWC12cne-o9u_79&export=download', '_parent');
-   }
+  const handleDownload = () => {
+    window.open('https://drive.google.com/u/1/uc?id=1Q7wzCfqI_aT_PwkPPqWC12cne-o9u_79&export=download', '_parent');
+  }
 
-   return (
-      <div className='bg-[#212529] text-[#798792] min-h-screen' id="home">
-         <Headroom>
-            <NavBar></NavBar>
-         </Headroom>
+  return (
+    <div className='bg-[#212529] text-[#798792] min-h-screen' id="home">
+      <Headroom>
+        <NavBar></NavBar>
+      </Headroom>
 
-         <Container>
-            <div className='banner pt-20' data-aos="fade-up" data-aos-duration="1500">
-               <div className="banner-content">
-                  <h1>Hello ! I&apos;m Protap Ghosh</h1>
-                  <h2>
-                     <span className='text-[#20C997]'>
-                        {text}
-                     </span>
-                     <Cursor />
+      <Container>
+        <div className='banner pt-20' data-aos="fade-up" data-aos-duration="1500">
+          <div className="banner-content">
+            <h1>Hello ! I&apos;m Protap Ghosh</h1>
+            <h2>
+              <span className='text-[#20C997]'>
+                {text}
+              </span>
+              <Cursor />
+            </h2>
+
+            <p className='lg:w-4/5 text-[18px] font-semibold'>
+              As a self-taught programmer, I have honed my skills in the MERN stack, demonstrating proficiency in JavaScript, HTML, and CSS. I possess a strong understanding of these technologies and strive to stay updated with the latest industry trends and advancements. My commitment to continuous learning allows me to adapt to evolving programming practices.
+            </p>
+
+            <a href="#contact" className='btn btn-outline border-[#20C997] hover:border-[#20C997] text-[16px] text-[#20C997] capitalize border-2 rounded-full px-8 mt-8 hover:bg-[#20C997]'>Hire Me</a>
+
+            <button onClick={handleDownload} className='btn border-none text-[16px] text-white capitalize border-2 rounded-full px-8 mt-8 ms-5 bg-[#20C997] hover:bg-[#1BAA80]'>Resume <FaDownload /></button>
+          </div>
+
+          <div className='banner-image' data-aos="fade-left" data-aos-duration="1500">
+            <div className='box'>
+              <div className='content'>
+                <img src={image} alt="Image" />
+                <div className='text'>
+                  <h2 className='relative text-3xl font-bold uppercase'>Protap Ghosh <br />
+                    <span className='text-base font-semibold'>MERN Stack Developer</span>
                   </h2>
-
-                  <p className='lg:w-4/5 text-[18px] font-semibold'>
-                     As a self-taught programmer, I have honed my skills in the MERN stack, demonstrating proficiency in JavaScript, HTML, and CSS. I possess a strong understanding of these technologies and strive to stay updated with the latest industry trends and advancements. My commitment to continuous learning allows me to adapt to evolving programming practices.
-                  </p>
-
-                  <a href="#contact" className='btn btn-outline border-[#20C997] hover:border-[#20C997] text-[16px] text-[#20C997] capitalize border-2 rounded-full px-8 mt-8 hover:bg-[#20C997]'>Hire Me</a>
-
-                  <button onClick={handleDownload} className='btn border-none text-[16px] text-white capitalize border-2 rounded-full px-8 mt-8 ms-5 bg-[#20C997] hover:bg-[#1BAA80]'>Resume <FaDownload /></button>
-               </div>
-
-               <div className='banner-image' data-aos="fade-left" data-aos-duration="1500">
-                  <div className='box'>
-                     <div className='content'>
-                        <img src={image} alt="Image" />
-                        <div className='text'>
-                           <h2 className='relative text-3xl font-bold uppercase'>Protap Ghosh <br />
-                              <span className='text-base font-semibold'>MERN Stack Developer</span>
-                           </h2>
-                           <a href="#contact" className='btn btn-outline border-[#6C757D] hover:border-[#6C757D] text-[16px] text-[#6C757D] capitalize border-2 rounded-full px-8 mt-5 hover:bg-[#6C757D]'>Hire Me</a>
-                        </div>
-                     </div>
-                  </div>
-               </div>
+                  <a href="#contact" className='btn btn-outline border-[#6C757D] hover:border-[#6C757D] text-[16px] text-[#6C757D] capitalize border-2 rounded-full px-8 mt-5 hover:bg-[#6C757D]'>Hire Me</a>
+                </div>
+              </div>
             </div>
-         </Container>
+          </div>
+        </div>
+      </Container>
 
-         <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-               background: {
-                  color: {
+      {/* <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
 
-                  },
-               },
-               fullScreen: {
-                  enable: true,
-                  zIndex: 1,
-               },
-               style: {
-                  position: "absolute",
-                  width: "100%",
-                  height: "100%"
-               },
-               fpsLimit: 120,
-               interactivity: {
-                  events: {
-                     onClick: {
-                        enable: false,
-                        mode: "push",
-                     },
-                     onHover: {
-                        enable: true,
-                        mode: "repulse",
-                     },
-                     resize: true,
-                  },
-                  modes: {
-                     push: {
-                        quantity: 4,
-                     },
-                     repulse: {
-                        distance: 150,
-                        duration: 0.4,
-                     },
-                  },
-               },
-               particles: {
-                  color: {
-                     value: "#00ccff",
-                  },
-                  links: {
-                     color: "#20C997",
-                     distance: 150,
-                     enable: true,
-                     opacity: 0.5,
-                     width: 1,
-                  },
-                  collisions: {
-                     enable: true,
-                  },
-                  move: {
-                     direction: "none",
-                     enable: true,
-                     outModes: {
-                        default: "bounce",
-                     },
-                     random: false,
-                     speed: 2,
-                     straight: false,
-                  },
-                  number: {
-                     density: {
-                        enable: true,
-                        area: 800,
-                     },
-                     value: 80,
-                  },
-                  opacity: {
-                     value: 0.5,
-                  },
-                  shape: {
-                     type: "circle",
-                  },
-                  size: {
-                     value: { min: 1, max: 5 },
-                  },
-               },
-               detectRetina: true,
-            }}
-         />
+            },
+          },
+          fullScreen: {
+            enable: true,
+            zIndex: 1,
+          },
+          style: {
+            position: "absolute",
+            width: "100%",
+            height: "100%"
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#00ccff",
+            },
+            links: {
+              color: "#20C997",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      /> */}
 
-         {/* Scroll Down Animation */}
-         <a href="#about-me">
-            <div className="scroll-down"></div>
-         </a>
+      {/* Scroll Down Animation */}
+      <a href="#about-me">
+        <div className="scroll-down"></div>
+      </a>
 
-         {/* Scroll Up */}
-         <ScrollToTop smooth top="400" style={{ backgroundColor: "#20c99685", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%", padding: "10px" }} />
-      </div>
-   );
+      {/* Scroll Up */}
+      <ScrollToTop smooth top="400" style={{ backgroundColor: "#20c99685", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%", padding: "10px" }} />
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import notFound from '../../assets/image/NotFound.jpg'
 
 const NotFound = () => {
-   return (
-      <div>
-         
+  return (
+    <div>
+      <div className="hero min-h-screen bg-[#20c99618]">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={notFound} className="lg:max-w-xl rounded-lg shadow-2xl" />
+
+          <div className='mr-10'>
+            <h1 className="text-6xl font-bold"><span className='text-[#20C997]'>Oops,</span> page not found</h1>
+            <p className="py-6 text-xl">Sorry, the page you are looking for could not be found</p>
+            <Link className="btn btn-outline border-[#1eb889] text-[#20C997]"><FaArrowLeft className='mr-2' /> Back To Home</Link>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default NotFound;
