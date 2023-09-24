b964d564d860147e4774084ffcfa788893d2298f
Protap Ghosh <protapg518@gmail.com>
2023-09-24 22:17:15 +0600

    updated dynamic data in footer

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
    return (
       <footer className="footer footer-center p-4 bg-[#343A40] text-[#8695a0] text-[17px] font-semibold">
          <div>
-            <p>Copyright © 2023 - All right reserved by Protap Ghosh</p>
+         <p>Copyright © {new Date().getFullYear()} - All right reserved by Protap Ghosh</p>
          </div>
       </footer>
    );
